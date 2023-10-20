edb6a59b79b816a106823a02d64808b788b5aaeb
grigol tvaradze <grigol.tvaradze@gmail.com>
2023-10-20 23:52:01 +0400

    davalebis meore nawili

---
+++ davaleba6/davaleba6-2/davaleba6-2/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba6_2
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a+b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static decimal Divide(decimal a, decimal b)
+        {
+            if(b == 0)
+            {
+                throw new ArgumentException("0 ze gayofa ar sheidzleba");
+            }
+
+            return  a / b;
+        }
+    }
+}
---
+++ davaleba6/davaleba6-2/davaleba6-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using davaleba6_2;
+
+int x = 8;
+int y = 6;
+
+int jami = Calculator.Add(x, y);
+Console.WriteLine("x + y tolia: " + jami);
+
+int sxvaoba = Calculator.Subtract(x, y);
+Console.WriteLine("x - y tolia: " + sxvaoba);
+
+int namravli = Calculator.Multiply(x, y);
+Console.WriteLine("x * y tolia: " + namravli);
+
+decimal gayofa = Calculator.Divide(x, y);
+Console.WriteLine("x / y tolia: " + gayofa);
