aecce272c0a5c6325e788852ea01c35091120f4b
grigol tvaradze <grigol.tvaradze@gmail.com>
2023-10-20 17:50:50 +0400

    bevri wvalebis mere me da chatgptm  gavaketet

---
+++ davaleba 3/davaleba3/davaleba3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] numbers = {645 , 14314, 8741 };
+
+int index = 1;
+
+int result = CalculateDigitSum(numbers, index);
+
+
+Console.WriteLine("elementis cifrebis jamia: " + result);
+
+
+static int CalculateDigitSum(int[] array, int index)
+{
+    int element = array[index];
+    int sum = 0;
+
+    while (element > 0)
+    {
+        int digit = element % 10;
+        sum += digit;
+        element /= 10;
+    }
+
+    return sum;
+}
