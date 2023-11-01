6ce86af01a2d325f2cd91fb61df32e4ba68ec87e
grigol tvaradze <grigol.tvaradze@gmail.com>
2023-11-02 00:50:44 +0400

    tolobis gadatvirtva da da explicit ze chatgpt mishvela dziritadi me vqeni magram chatgpt gareshe ver gavaketebdi gansakutrebit me6 punqts imedia sworadaa

---
+++ davaleba8/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1;
+
+Console.WriteLine("Enter the lengths of the first triangle's sides:");
+double side1 = Convert.ToDouble(Console.ReadLine());
+double side2 = Convert.ToDouble(Console.ReadLine());
+double side3 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the lengths of the second triangle's sides:");
+double sideA = Convert.ToDouble(Console.ReadLine());
+double sideB = Convert.ToDouble(Console.ReadLine());
+double sideC = Convert.ToDouble(Console.ReadLine());
+
+try
+{
+    Triangle triangle1 = new Triangle(side1, side2, side3);
+    Triangle triangle2 = new Triangle(sideA, sideB, sideC);
+
+    Console.WriteLine($"Area of the first triangle: {triangle1.GetArea()}");
+    Console.WriteLine($"Area of the second triangle: {triangle2.GetArea()}");
+
+    Triangle sumTriangle = triangle1 + triangle2;
+    Console.WriteLine("Sum of two triangles:");
+
+    Console.WriteLine($"Area: {sumTriangle.GetArea()}");
+    Console.WriteLine($"Side1: {sumTriangle.Side1}");
+    Console.WriteLine($"Side2: {sumTriangle.Side2}");
+    Console.WriteLine($"Side3: {sumTriangle.Side3}");
+
+
+    if (IsRightTriangle(sumTriangle))
+    {
+        Console.WriteLine("The sum triangle is a right triangle.");
+    }
+    else
+    {
+        Console.WriteLine("The sum triangle is not a right triangle.");
+    }
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+static bool IsRightTriangle(Triangle triangle)
+{
+    double[] sides = { triangle.Side1, triangle.Side2, triangle.Side3 };
+    Array.Sort(sides);
+    return Math.Abs(sides[0] * sides[0] + sides[1] * sides[1] - sides[2] * sides[2]) < double.Epsilon;
+}
+
---
+++ davaleba8/ConsoleApp1/ConsoleApp1/Triangle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Triangle
+    {
+        public double Side1 { get; private set; }
+        public double Side2 { get; private set; }
+        public double Side3 { get; private set; }
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            if (IsValidTriangle(side1, side2, side3))
+            {
+                Side1 = side1;
+                Side2 = side2;
+                Side3 = side3;
+            }
+            else
+            {
+                throw new ArgumentException("nebismieri ori gverdis jami meti unda iyos mesameze!");
+            }
+        }
+
+        public double GetArea()
+        {
+            double s = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(s * (s - Side1) * (s - Side2) * (s - Side3));
+        }
+
+        public double GetPerimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+
+        public static bool operator ==(Triangle t1, Triangle t2)
+        {
+            return Math.Abs(t1.GetArea() - t2.GetArea()) < double.Epsilon;
+        }
+
+        public static bool operator !=(Triangle t1, Triangle t2)
+        {
+            return !(t1 == t2);
+        }
+
+        public static bool operator >(Triangle t1, Triangle t2)
+        {
+            return t1.GetArea() > t2.GetArea();
+        }
+
+        public static bool operator <(Triangle t1, Triangle t2)
+        {
+            return t1.GetArea() < t2.GetArea();
+        }
+
+        public static Triangle operator +(Triangle t1, Triangle t2)
+        {
+            double newSide1 = Math.Sqrt(t1.Side1 * t1.Side1 + t2.Side1 * t2.Side1);
+            double newSide2 = Math.Sqrt(t1.Side2 * t1.Side2 + t2.Side2 * t2.Side2);
+            double newSide3 = Math.Sqrt(newSide1 * newSide1 + newSide2 * newSide2); 
+            return new Triangle(newSide1, newSide2, newSide3);
+        }
+
+        public static explicit operator Triangle(double area)
+        {
+            double side1 = Math.Sqrt((2 * area) / Math.Sqrt(3));
+            double side2 = side1;
+            double side3 = side1 * Math.Sqrt(3);
+            return new Triangle(side1, side2, side3);
+        }
+
+        private static bool IsValidTriangle(double a, double b, double c)
+        {
+            return a + b > c && a + c > b && b + c > a;
+        }
+    }
+}
